d224ac18f35f780d18bb09ab65993aa351a77a71
Raj kumar <glrajv369@gmail.com>
2024-05-10 00:13:00 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "installation-guide-site",
+  base: "/installation-guide-site",
   plugins: [react()],
 })
